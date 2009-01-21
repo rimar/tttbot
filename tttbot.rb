e566e987a6a893c08538741b2bb78ba5142f32dd
Yuval Rimar <yuval.rimar@gmail.com>
2009-01-22 00:18:52 +0200

    add init method

--- tttbot.rb
+++ tttbot.rb
@@ -1,2 +1,24 @@
 class TTTBot
+
+  def initialize
+    @players = {}
+  end
+
+  def introduce(name, player)
+    @players[name] = player
+  end
+
+  def run
+    if @players.size != 2 
+      raise "2 players required" 
+    end
+
+    return @players.keys[0]
+  end
+
 end
+
+def init
+  return TTTBot.new
+end
+
